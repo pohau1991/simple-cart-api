322afe22c7fc337ad291f53dd968fa878cfb32cf
Po <po@getepicured.com>
2021-04-14 10:43:42 -0400

    update routing for initial test

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+/**Route::group(['middleware' => 'auth'], function(){
+    Route::get('/user', [
+        'uses' => 'UserController@getUser'
+    ])
+});**/
+Route::group(['prefix' => 'items'], function(){
+    Route::put('/', [
+        'uses' => 'ItemController@getItems',
+    ]);
+});
